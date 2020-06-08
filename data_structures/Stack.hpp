82ee7ca8e0241ad93e957eddc670ce3862516283
Ashu Kher <ashukher1@gmail.com>
2020-06-08 13:48:16 -0400

    added comments for the functions

--- data_structures/Stack.hpp
+++ data_structures/Stack.hpp
@@ -40,26 +40,51 @@ class Stack {
                                     const Stack<Type>& stack);
 };
 
+/**
+ * Default constructor for the stack
+ *  Sets the capacity to a default value of 10
+ */
 template <typename Type>
 Stack<Type>::Stack() {
   this->capacity = 10;
   this->size = 0;
 }
 
+/**
+ * Constuctor that takes a value for the capacity of the stack
+ * @param capacity - max capacity of the stack
+ */
 template <typename Type>
 Stack<Type>::Stack(int capacity) {
-  this->capacity = capacity;
-  this->size = 0;
+  if (capacity > 0) {
+    this->capacity = capacity;
+    this->size = 0;
+  } else {
+    exit(EXIT_FAILURE);
+  }
 }
 
+/**
+ * Default destructor
+ *  Note: Not really needed because STL and compiler handles garbage
+ *        collection
+ */
 template <typename Type>
 Stack<Type>::~Stack() {}
 
+/**
+ * Returns the number of elements in the stack
+ * @return - number of elements in the stack
+ */
 template <typename Type>
 size_t Stack<Type>::get_size() {
   return size;
 }
 
+/**
+ * Pushes an element onto the stack
+ * @param item - new item to put onto the stack
+ */
 template <typename Type>
 void Stack<Type>::push(Type item) {
   if (!is_full()) {
@@ -71,6 +96,10 @@ void Stack<Type>::push(Type item) {
   }
 }
 
+/**
+ * Removes and returns the top element from the stack
+ * @return - the removed element from the stack
+ */
 template <typename Type>
 Type Stack<Type>::pop() {
   if (!is_empty()) {
@@ -84,6 +113,10 @@ Type Stack<Type>::pop() {
   }
 }
 
+/**
+ * Returns the top element from the stack
+ * @return - the top element in the stack
+ */
 template <typename Type>
 Type Stack<Type>::get_top() {
   if (!is_empty()) {
@@ -94,16 +127,31 @@ Type Stack<Type>::get_top() {
   }
 }
 
+/**
+ * Returns if the stack is empty
+ * @return - boolean if the stack is empty
+ */
 template <typename Type>
 bool Stack<Type>::is_empty() {
   return size == 0;
 }
 
+/**
+ * Returns if the stack is full
+ * @return - boolean if the stack is full
+ */
 template <typename Type>
 bool Stack<Type>::is_full() {
   return size == capacity;
 }
 
+/**
+ * Overloaded the << operator for output streams
+ * @param output - reference to the output stream
+ * @param stack - constant reference to a stack to put in the stream
+ *
+ * @return output - using the referenced output stream
+ */
 template <typename Type>
 std::ostream& operator<<(std::ostream& output, const Stack<Type>& stack) {
   output << "Stack: [";
--- data_structures/main.cpp
+++ data_structures/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <string> 
 
 #include "Stack.hpp"
 
 int main() {
   Stack<int> simp(20);
   Stack<int> simp2;
+
   return 0;
 }
