0850c330217fea39f4a93f17c5b29b9ec1f2884a
Ashu Kher <ashukher1@gmail.com>
2020-06-08 09:04:40 -0400

    Update Stack.hpp

--- data_structures/Stack.hpp
+++ data_structures/Stack.hpp
@@ -6,14 +6,22 @@ template <typename Type>
 class Stack {
  private:
   std::vector<Type> data;
-  int size = 0;
+  size_t size = 0;
 
  public:
   Stack();
   ~Stack();
+
+  std::vector<Type> get_data(); 
+  void push(Type item); 
 };
 
 // Stack::Stack() {}
 // Stack::~Stack() {}
+
+template <typename Type>
+void Stack::push(Type item) {
+  data.push_back(item); 
+}
 
 #endif
