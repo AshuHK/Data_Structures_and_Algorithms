128704137bd37c4109c9e45d28940e1b0867b3d8
Ashu Kher <ashukher1@gmail.com>
2020-06-08 10:09:44 -0400

    figured out tempaltes finally

--- data_structures/Stack.hpp
+++ data_structures/Stack.hpp
@@ -8,25 +8,16 @@ template <typename Type>
 class Stack {
  private:
   std::vector<Type> data;
-  size_t size = 0;
+  size_t size;
 
  public:
   Stack() = default;
-  ~Stack() { std::cout << "The stack has been destroyed" << std::endl; }
-
-  size_t get_size() { return size; }
-
-  void push(Type item) {
-    data.push_back(item);
-    size++;
-  }
-
-  Type pop() {
-    Type old = data[size - 1];
-    data.pop_back();
-    size--;
-    return old;
-  }
+  ~Stack();
 };
+
+template <typename Type>
+Stack<Type>::~Stack() {
+  std::cout << "The stack has been destroyed" << std::endl;
+}
 
 #endif
--- data_structures/main.cpp
+++ data_structures/main.cpp
@@ -7,12 +7,6 @@ int main() {
   std::vector<int> data{1, 2, 3, 4, 5};
 
   Stack<int> simp; 
-  simp.push(1); 
-  simp.push(2); 
-  std::cout << simp.get_size() << std::endl; 
-
-  std::cout << simp.pop() << std::endl; 
-  std::cout << simp.get_size() << std::endl; 
 
   return 0;
 }
