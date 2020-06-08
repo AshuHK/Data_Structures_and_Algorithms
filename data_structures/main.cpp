d5f0a127f206a145f12fa263c811ee5a440be721
Ashu Kher <ashukher1@gmail.com>
2020-06-08 19:20:52 -0400

    just showing off the stacks and queues

--- data_structures/main.cpp
+++ data_structures/main.cpp
@@ -20,5 +20,22 @@ int main() {
     std::cout << queue << std::endl;
   }
 
+  std::cout << std::endl; 
+  
+  Stack<int> stack; 
+
+  for (int i = 0; i < 10; i++) {
+    std::cout << stack << std::endl;
+    stack.push(i); 
+  }
+
+  std::cout << stack << std::endl;
+
+  for (int i = 0; i < 10; i++) {
+    stack.pop(); 
+    std::cout << stack << std::endl;
+  }
+
+
   return 0;
 }
