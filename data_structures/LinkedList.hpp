91e9d08bbb7ca76a764c5ab739e20e4459cab1f9
Ashu Kher <ashukher1@gmail.com>
2020-06-09 10:37:46 -0400

    Update LinkedList.hpp

--- data_structures/LinkedList.hpp
+++ data_structures/LinkedList.hpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector> 
+#include <vector>
 
 #ifndef LINKEDLIST_HPP
 #define LINKEDLIST_HPP
@@ -12,7 +12,7 @@ template <typename Type1, typename Type2>
 std::ostream& operator<<(std::ostream& output,
                          const LinkedNode<Type1, Type2>& node);
 
-// complete definition for the linked list node 
+// complete definition for the linked list node
 template <typename Type1, typename Type2>
 struct LinkedNode {
   LinkedNode* next = nullptr;
@@ -51,33 +51,42 @@ bool LinkedNode<Type1, Type2>::operator==(
   }
 }
 
-template <typename Type1, typename Type2> 
+template <typename Type1, typename Type2>
+class LinkedList;
+
+template <typename Type1, typename Type2>
+std::ostream& operator<<(std::ostream& output,
+                         const LinkedList<Type1, Type2> list);
+
+template <typename Type1, typename Type2>
 class LinkedList {
-  private: 
-    LinkedNode* head = nullptr; 
-    size_t size = 0; 
+ private:
+  LinkedNode* head = nullptr;
+  size_t size = 0;
 
-  public: 
-    // constructors 
-    LinkedList() = default; 
-    LinkedList(std::vector<LinkedNode<Type1, Type2> nodes);  
+ public:
+  // constructors
+  LinkedList() = default;
+  LinkedList(std::vector < LinkedNode<Type1, Type2> nodes);
 
-    // setters and getters 
-    size_t get_size(); 
-    LinkedNode<Type1, Type2> * get_head(); 
+  // setters and getters
+  size_t get_size();
+  LinkedNode<Type1, Type2>* get_head();
 
-    // Linked List operations
-    void add_back(Type1 key, Type2 value); 
-    void add_front(Type1 key, Type2 value); 
+  // Linked List operations
+  void add_back(Type1 key, Type2 value);
+  void add_front(Type1 key, Type2 value);
 
-    LinkedNode<Type1, Type2> & search(Type1 key); 
+  LinkedNode<Type1, Type2>& search(Type1 key);
 
-    LinkedNode<Type1, Type2> remove_front(); 
-    LinkedNode<Type1, Type2> remove_back(); 
-    
-    void update_value(Type1 key); 
+  LinkedNode<Type1, Type2> remove_front();
+  LinkedNode<Type1, Type2> remove_back();
 
-}; 
+  void update_value(Type1 key);
 
+  // overloaded operations
+  friend std::ostream& operator<<<>(std::ostream& output,
+                                    const LinkedList<Type1, Type2>& list);
+};
 
 #endif
